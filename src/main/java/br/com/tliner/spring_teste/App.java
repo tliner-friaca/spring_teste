fedb8ec72b0cda25fdac11100aa4af18fcdddda3
tliner.castro <tliner.castro@10WI7-0591.LELLO.CORP>
2019-07-29 16:38:24 -0300

    inicial

---
+++ src/main/java/br/com/tliner/spring_teste/App.java
@@ -0,0 +1,13 @@
+package br.com.tliner.spring_teste;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
